d6d857fb4f2861511fe8939f5f0b8367d713f03c
청소부장 <washingmanager2@gmail.com>
2025-03-31 11:16:33 +0900

    Update keyword.js

--- keyword.js
+++ keyword.js
@@ -2,8 +2,11 @@ module.exports = {
   getKeywords: () => {
     // 원하는 키워드 리스트
     const keywords = [    // 나라이름별
-    '우크라이나', '우크라', '북한', '한국', 
-    return keywords;
+      '우크라이나', 
+      '우크라', 
+      '북한', 
+      '한국'
+    ]; // 배열 닫기
+    return keywords; // return은 배열 정의 밖에 위치
   }
 };
-
