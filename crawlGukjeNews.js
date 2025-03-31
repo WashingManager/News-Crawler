7dd90a5f9c5d4e633ec4d386e3fdd95d639bc4fd
청소부장 <washingmanager2@gmail.com>
2025-03-31 11:00:59 +0900

    Update crawlGukjeNews.js

--- crawlGukjeNews.js
+++ crawlGukjeNews.js
@@ -48,7 +48,11 @@ async function processArticle($, element, keywords, existingNews) {
 async function crawlPage(url, keywords, existingNews, page = 1) {
   try {
     const fullUrl = `${url}&page=${page}`;
-    const response = await axios.get(fullUrl);
+    const response = await axios.get(fullUrl, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    });
     const $ = cheerio.load(response.data);
     const elements = $('ul.type2 li');
     const newArticles = [];
